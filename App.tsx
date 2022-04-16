ba481ad9f2f67fdfe523bad75fd0155a9ad9b9ca
Igor Belem <igortbelem@gmail.com>
2022-04-15 21:18:56 -0400

    Removed random test code

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-    let a;
     return (
         <View style={styles.container}>
             <Text>Bolao</Text>
