a5e874d12fd4bdc9b8ee68c49a304fcb479e1bb0
Igor Belem <igortbelem@gmail.com>
2022-04-17 17:20:44 -0400

    Added assets export

---
+++ assets.ts
@@ -0,0 +1,25 @@
+import Login from "./assets/media/login.png";
+import Register from "./assets/media/register.png";
+import ForgotPassword from "./assets/media/forgot.png";
+import Crown from "./assets/media/crown.png";
+import Burger from "./assets/media/burger.png";
+import Guess from "./assets/media/guess.png";
+import Rank from "./assets/media/rank.png";
+import Wins from "./assets/media/wins.png";
+import Rounds from "./assets/media/rounds.png";
+import Rules from "./assets/media/rules.png";
+
+export const Assets = {
+    media: {
+        login: Login,
+        register: Register,
+        forgotPass: ForgotPassword,
+        crown: Crown,
+        burger: Burger,
+        guess: Guess,
+        rank: Rank,
+        wins: Wins,
+        rules: Rules,
+        rounds: Rounds,
+    },
+};
