95a6e3e5e7b54ee26b5d6b103dca5feae85f5e0d
Igor Belem <igortbelem@gmail.com>
2022-04-17 17:26:45 -0400

    Added spinner to button, loading state

--- src/components/Button/SimpleButton.tsx
+++ src/components/Button/SimpleButton.tsx
@@ -1,21 +1,39 @@
-import { TouchableOpacity, Text, TextStyle, ViewStyle } from "react-native";
+import {
+    TouchableOpacity,
+    Text,
+    TextStyle,
+    ViewStyle,
+    ActivityIndicator,
+} from "react-native";
 type SimpleButtonProps = {
     label: string;
     onPress: () => void;
     buttonStyle?: ViewStyle;
     labelStyle?: TextStyle;
+    isLoading?: boolean;
 };
 export default function SimpleButton(props: SimpleButtonProps) {
     const buttonStyle = props?.buttonStyle
-        ? { padding: 16, borderRadius: 8, backgroundColor: "tomato", ...props.buttonStyle }
+        ? { ...props.buttonStyle }
         : { padding: 16, borderRadius: 8, backgroundColor: "tomato" };
     const labelStyle = props?.labelStyle
-        ? { color: "white", fontSize: 16, ...props.labelStyle }
+        ? { ...props.labelStyle }
         : { color: "white", fontSize: 16 };
     labelStyle;
     return (
-        <TouchableOpacity style={buttonStyle} onPress={props.onPress}>
-            <Text style={labelStyle}>{props.label}</Text>
+        <TouchableOpacity
+            disabled={props.isLoading}
+            style={buttonStyle}
+            onPress={props.onPress}
+        >
+            {props.isLoading ? (
+                <ActivityIndicator
+                    size="large"
+                    color={labelStyle.color ?? "#000"}
+                ></ActivityIndicator>
+            ) : (
+                <Text style={labelStyle}>{props.label}</Text>
+            )}
         </TouchableOpacity>
     );
 }
